7ddd302c24b58192424ca57a03fbc5d40277d3d3
vishal <itsmevishal360@gmail.com>
2025-11-23 20:03:55 +0530

    solve build failed

--- src/routes/RouterConfig.tsx
+++ src/routes/RouterConfig.tsx
@@ -1,4 +1,3 @@
-import React from 'react'
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import LandingPage from '../pages/landing-page/LandingPage'
 import Login from '../pages/auth/Login'
