ed99c1c4ebb69182c8b080dcad4d12965e60a053
vishal <itsmevishal360@gmail.com>
2025-12-20 19:56:09 +0530

    add bottom navbar

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,14 @@
+import { BrowserRouter } from 'react-router-dom'
+import BottomNavbar from './components/navbar/BottomNavbar'
 import RouterConfig from './routes/RouterConfig'
 import { ToastContainer } from 'react-toastify'
+import { useShouldShowNavbar } from './hooks/useShouldShowNavbar'
 
-const App = () => {
+const AppContent = () => {
+  const showNavbar = useShouldShowNavbar()
+
   return (
-    <div className='flex justify-center min-h-screen bg-primary'>
+    <div className='flex justify-center bg-primary'>
       <RouterConfig />
       <ToastContainer
         position="bottom-right"
@@ -16,7 +21,20 @@ const App = () => {
         draggable
         theme="light"
       />
+      {showNavbar && (
+        <div className='fixed bottom-0 w-full md:w-auto'>
+          <BottomNavbar />
+        </div>
+      )}
     </div>
+  )
+}
+
+const App = () => {
+  return (
+    <BrowserRouter>
+      <AppContent />
+    </BrowserRouter>
   )
 }
 
